0b73044df863ec5fc3536148cad4f8d51ab67e37
hanliumaozhi <hanliumaozhi@gmail.com>
2015-07-07 15:13:44 +0800

    add user model

--- louqa/user/models.py
+++ louqa/user/models.py
@@ -3,6 +3,7 @@
 
 from ..dbs import db
 from sqlalchemy import Column
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,3 +12,12 @@ class User(db.Model):
 
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String(32), nullable=False, unique=True)
+    email = Column(db.String(64), nullable=False, unique=True)
+
+    __password = Column(db.String(128))
+
+    def set_password(self, password):
+        self.__password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.__password, password)
